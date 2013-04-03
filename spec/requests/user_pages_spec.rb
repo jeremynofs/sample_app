2e0d0dd7c6a4afdf3dadd3b5879a37a1856f9b59
Jeremy Nofs <jeremy.nofs@gmail.com>
2013-04-02 22:03:12 -0400

    Add up to section 9.1.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -9,7 +9,6 @@ describe "User pages" do
  
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
-
   end
 
 	describe "profile page" do
@@ -37,7 +36,6 @@ describe "User pages" do
         it { should have_selector('title', text: 'Sign up') }
         it { should have_content('error') }
       end
-
     end
 
     describe "with valid information" do
@@ -69,6 +67,23 @@ describe "User pages" do
 
         it { should have_link('Sign out') }
       end
+    end
+  end
+
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit edit_user_path(user) }
+
+    describe "page" do
+      it { should have_selector('h1',    text: "Update your profile") }
+      it { should have_selector('title', text: "Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+
+      it { should have_content('error') }
     end
   end
 end
