aa278c600ccdcedfcd6d495342e2b79d50c00ea8
Jeremy Nofs <jeremy.nofs@gmail.com>
2013-04-10 15:49:26 -0400

    Finish authorization

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,9 @@ class SessionsController < ApplicationController
 	def create
 	  user = User.find_by_email(params[:session][:email].downcase)
 	  if user && user.authenticate(params[:session][:password])
-	    # Sign the user in and redirect to the user's show page.
+	    # Sign the user in and redirects to the user's requested page.
 	    sign_in user
-	    redirect_to user
+	    redirect_back_or user
 	  else
 	    # Create an error message and re-render the signin form.
 	    flash.now[:error] = "Invalid email/password combination!"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
   def new
   	@user = User.new
   end
@@ -19,16 +22,31 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      #handle successful update
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
     else
       render 'edit'
     end
   end
+
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 
 end
