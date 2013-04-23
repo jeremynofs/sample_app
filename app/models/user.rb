d1ec9fa87b7220b7a8c13b331a20c4ae51b9ecd6
Jeremy Nofs <jeremy.nofs@gmail.com>
2013-04-23 17:01:00 -0400

    Add microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
+  has_many :microposts, dependent: :destroy
   has_secure_password
 
   # Callbacks
